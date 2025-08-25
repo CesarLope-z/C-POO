91234cb98eea504a5c0f3ef73c9114b5cb24c5d1
cesar lopez <cesarlopezortiz902@gmail.com>
2025-08-25 16:01:51 -0600

    just a console write

--- Ejemplo_prueba/Program.cs
+++ Ejemplo_prueba/Program.cs
@@ -10,6 +10,9 @@ namespace Ejemplo_prueba
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola, mundo!");
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
